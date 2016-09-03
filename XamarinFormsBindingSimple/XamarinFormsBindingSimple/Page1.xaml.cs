8e08b068259d64f236d7ea0062a137d556bafda0
kobake <kobake@users.sourceforge.net>
2016-09-03 18:48:38 +0900

    Binding MainText property of Page1

--- XamarinFormsBindingSimple/XamarinFormsBindingSimple/Page1.xaml.cs
+++ XamarinFormsBindingSimple/XamarinFormsBindingSimple/Page1.xaml.cs
@@ -10,9 +10,18 @@ namespace XamarinFormsBindingSimple
 {
 	public partial class Page1 : ContentPage
 	{
+		public string MainText { get; set; } = "Hello";
+
 		public Page1()
 		{
 			InitializeComponent();
+			this.BindingContext = this;
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			this.MainText += "!";
+			this.OnPropertyChanged("MainText");
 		}
 	}
 }
